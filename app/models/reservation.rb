d9474618308831132f91cb37af7fd3d47809ad2e
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-04-25 11:18:51 +0630

    create the validation for the users

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :shoes
   belongs_to :users
+  validates :count, presence: true
+  validates :size, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
      has_many :shoes
-     has_many  :reservations
+     has_many :reservations
+     validates :name, presence: true, uniqueness: { message: 'Must be unique'},
+                length: { in: 3..250, message: 'Must be a string of at least 3 characters' }
+     validates :name, :email, :password, presence: true
+     validates :password, length: { minimum: 8 }
 end
