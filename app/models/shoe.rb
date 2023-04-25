18d2c16186ee49c0ba417aa75a80bf4064461cba
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-04-25 11:09:56 +0630

    create the associations between models

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,3 +1,7 @@
 class Shoe < ApplicationRecord
   belongs_to :users
+  has_many :images
+  has_many :colors
+  has_many :size
+  has_many :reservations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+     has_many :shoes
+     has_many  :reservations
 end
