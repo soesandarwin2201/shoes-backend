63d1ee226859812d053b73cf384fd8845f065650
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-04-27 10:45:41 +0630

    Modify the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+   
+  post '/login', to: 'authentication#login'
+  post '/signup', to: 'users#create'
   # Defines the root path route ("/")
   root "shoes#index"
   get "reservation", to: "reservations#index"
-  resources :users
+  resources :users, param: :_username
   resources :shoes do 
   resources :reservations
   end
