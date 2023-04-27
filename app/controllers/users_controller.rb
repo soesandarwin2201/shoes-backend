de6d64619efa636b190084321df50cc659fbde0c
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-04-27 11:28:01 +0630

    create the authentication login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,30 @@
 class UsersController < ApplicationController
+  before_action :authorize_request, except: :create
+
   def index
-    @users = User.all
+    @users = current_user
     render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user, status: :ok
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
+                     user: @user }, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
